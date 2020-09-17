5b98c46987b83ea16d209bf2b4d8eac978f9a60d
muvirag <a.malatinszki@gmail.com>
2020-09-17 17:01:10 +0200

    Import Header component into App.js

--- dicksit/src/App.js
+++ dicksit/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">Header</div>
+      <div className="header">
+        <Header name="Bella"></Header>
+      </div>
       <div className="game-area">GameArea</div>
       <div className="scoreboard">Scoreboard</div>
       <div className="hand">Hand</div>

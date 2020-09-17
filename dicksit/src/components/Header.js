108fd1f010953fb6015fbc926bbf151ca5815045
muvirag <a.malatinszki@gmail.com>
2020-09-17 17:50:02 +0200

    Display username in Header Toolbar

--- dicksit/src/components/Header.js
+++ dicksit/src/components/Header.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import Typography from '@material-ui/core/Typography';
 
 function Header(props) {
   return (
     <div>
       <AppBar>
         <Toolbar>
-          Dicksit
+          <Typography variant="h6">Dicksit</Typography>
           <IconButton edge="end" aria-label="Current user">
             <AccountCircle />
           </IconButton>
+          <Typography variant="p">{props.name}</Typography>
           <IconButton edge="end" aria-label="Menu">
             <MenuIcon />
           </IconButton>

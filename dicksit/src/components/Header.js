b1c0781afeecac3516bdca14f03cb795419ffa9d
muvirag <a.malatinszki@gmail.com>
2020-09-17 18:05:26 +0200

    Style Header elements

--- dicksit/src/components/Header.js
+++ dicksit/src/components/Header.js
@@ -3,14 +3,35 @@ import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  accountIcon: {
+    padding: '1em',
+  },
+});
 
 function Header(props) {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar>
         <Toolbar>
-          <Typography variant="h6">Dicksit</Typography>
-          <IconButton edge="end" aria-label="Current user">
+          <Typography variant="h6" className={classes.title}>
+            Dicksit
+          </Typography>
+          <IconButton
+            edge="end"
+            aria-label="Current user"
+            className={classes.accountIcon}
+          >
             <AccountCircle />
           </IconButton>
           <Typography variant="p">{props.name}</Typography>

5d5eb6dac603e92fcfb770f8c53806bfdb47ced8
muvirag <a.malatinszki@gmail.com>
2020-09-17 21:52:18 +0200

    Implement score table in Scoreboard component

--- dicksit/src/components/Scoreboard.js
+++ dicksit/src/components/Scoreboard.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
-const Scoreboard = () => {
+const Scoreboard = (props) => {
   return (
     <div>
-      <div></div>
+      <TableContainer component={Paper}>
+        <Table aria-label="Scoreboard">
+          <TableBody>
+            {props.scores.map((score) => (
+              <TableRow key={score.name}>
+                <TableCell component="th" scope="row">
+                  {score.name}
+                </TableCell>
+                <TableCell align="right">{score.points}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };

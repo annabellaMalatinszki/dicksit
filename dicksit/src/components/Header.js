d17e25e554cd3cf2ae870b0e521a43c53e5716cc
muvirag <a.malatinszki@gmail.com>
2020-09-17 17:45:08 +0200

    Add account circle to Header Toolbar

--- dicksit/src/components/Header.js
+++ dicksit/src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-const Header = (props) => {
+function Header(props) {
   return (
     <div>
       <AppBar>
-        <Toolbar>Dicksit</Toolbar>
+        <Toolbar>
+          Dicksit
+          <IconButton edge="end" aria-label="Current user">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
       </AppBar>
     </div>
   );
-};
+}
 
 export default Header;

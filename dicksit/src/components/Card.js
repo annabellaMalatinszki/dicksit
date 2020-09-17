6359168d2caaab19feec7d22d1c345be77430bf6
muvirag <a.malatinszki@gmail.com>
2020-09-17 22:13:42 +0200

    Create Card component for displaying card images

---
+++ dicksit/src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { CardMedia, CardActionArea, Card as MUICard } from '@material-ui/core';
+
+const Card = ({ cardName }) => {
+  return (
+    <div>
+      <MUICard raised>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={cardName}
+            image={`${process.env.PUBLIC_URL}pics/${cardname}`}
+            title={cardName}
+          ></CardMedia>
+        </CardActionArea>
+      </MUICard>
+    </div>
+  );
+};
+
+export default Card;

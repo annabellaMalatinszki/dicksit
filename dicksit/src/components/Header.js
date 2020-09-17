17f952105ba9e80a1acb97ad463c0684561733dd
muvirag <a.malatinszki@gmail.com>
2020-09-17 17:00:43 +0200

    Add game title to header in AppBar MUI component

---
+++ dicksit/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const Header = (props) => {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>Dicksit</Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;

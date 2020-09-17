0a41c78a5bc7998583a2c629ae37963d16935428
muvirag <a.malatinszki@gmail.com>
2020-09-17 16:32:28 +0200

    Create basic css grid layout

--- dicksit/src/App.js
+++ dicksit/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">Header</div>
+      <div className="game-area">GameArea</div>
+      <div className="scoreboard">Scoreboard</div>
+      <div className="hand">Hand</div>
+    </div>
+  );
 }
 
 export default App;

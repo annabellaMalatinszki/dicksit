4c8bfe09333ba9658166912d1da5006eb031e8ff
muvirag <a.malatinszki@gmail.com>
2020-09-18 10:29:00 +0200

    Add Scoring and Rules buttons to Sidebar component

--- dicksit/src/components/SideBar.js
+++ dicksit/src/components/SideBar.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import Scoreboard from './Scoreboard';
+import { Button } from '@material-ui/core';
+import { makeStyle, makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles({
+  button: {
+    display: 'block',
+    width: '100%',
+    margin: '1em 0',
+  },
+});
 
 const SideBar = ({ scores }) => {
-  return <Scoreboard scores={scores} />;
+  const classes = useStyle();
+
+  return (
+    <div>
+      <Scoreboard scores={scores} />
+      <div>
+        <Button variant="contained" className={classes.button}>
+          Scoring
+        </Button>
+        <Button variant="contained" className={classes.button}>
+          Rules
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;

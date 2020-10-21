8fb8019eed5a32d432381cd59db3966b12e5223d
muvirag <a.malatinszki@gmail.com>
2020-10-21 17:10:25 +0200

    Implement request/response to test API connection

--- dicksit/client/src/App.js
+++ dicksit/client/src/App.js
@@ -38,23 +38,76 @@ const gameAreaCards = [
   { id: '12.png' },
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <Header name="Bella"></Header>
+class App extends React.Component {
+  state = {
+    respone: '',
+    post: '',
+    responseToPost: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({response: res.express}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+
+    return body;
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({post: this.state.post}),
+    });
+
+    const body = await response.text();
+
+    this. setState({responseToPost: body});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <Header name="Bella"></Header>
+        </div>
+        <div className="game-area">
+          <GameArea cards={gameAreaCards} />
+        </div>
+        <div className="side-bar">
+          <SideBar scores={scores} />
+        </div>
+        <div className="hand">
+          <Hand cards={userCards} />
+        </div>
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Psot to server</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
       </div>
-      <div className="game-area">
-        <GameArea cards={gameAreaCards} />
-      </div>
-      <div className="side-bar">
-        <SideBar scores={scores} />
-      </div>
-      <div className="hand">
-        <Hand cards={userCards} />
-      </div>
-    </div>
   );
+}
 }
 
 export default App;

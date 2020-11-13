15cefb1069dd6e84b780db5f92a502bb458e4ded
muvirag <a.malatinszki@gmail.com>
2020-11-13 09:47:20 +0100

    Send shuffled and dealt card data from server to client

--- dicksit/server.js
+++ dicksit/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mockdata = require('./mockdata.js');
+const cardHandler = require('./cardHandler.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// TODO This should not come from mockdata
+// TODO Cardpictures should be relocated, not all sent to the client for no reason
+const deck = mockdata.userCards;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/init', (req, res) => {
-  res.send({scores: mockdata.scores, userCards: mockdata.userCards, gameAreaCards: mockdata.gameAreaCards});
+  res.send({scores: mockdata.scores, userCards: cardHandler.dealHand(deck, 1)[0], gameAreaCards: mockdata.gameAreaCards});
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

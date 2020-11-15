2a65ca3edd2ca36a8a2a045564ef9e650ac71549
muvirag <a.malatinszki@gmail.com>
2020-11-15 12:13:31 +0100

    Add routing to App component for signin and game routes

--- dicksit/client/src/App.js
+++ dicksit/client/src/App.js
@@ -1,51 +1,20 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import SideBar from './components/SideBar';
-import Hand from './components/Hand';
-import GameArea from './components/GameArea';
+import { Route, Switch } from 'react-router-dom';
+import Game from './components/Game';
+import SignIn from './components/SignIn';
 
 class App extends React.Component {
-  state = {
-    scores: [],
-    userCards: [],
-    gameAreaCards: []
-  };
-
-  componentDidMount() {
-    this.callApi('init')
-      .then(res => {
-        this.setState({scores: res.scores, userCards: res.userCards, gameAreaCards: res.gameAreaCards});
-      }
-        )
-      .catch(err => console.log(err));
-  }
-
-  callApi = async (dest) => {
-    const response = await fetch(`/api/${dest}`);
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-
-    return body;
-  }
-
   render() {
     return (
       <div className="App">
-        <div className="header">
-          <Header name="Bella"></Header>
-        </div>
-        <div className="game-area">
-          <GameArea cards={this.state.gameAreaCards} />
-        </div>
-        <div className="side-bar">
-          <SideBar scores={this.state.scores} />
-        </div>
-        <div className="hand">
-          <Hand cards={this.state.userCards} />
+        <div>
+          <Switch>
+            <Route 
+              exact path='/game' 
+              component={Game} 
+            />
+            <Route path='/' component={SignIn}/>
+          </Switch>
         </div>
       </div>
   );

fafb77feada9b7ea5635f5d3c7d796b396439418
muvirag <a.malatinszki@gmail.com>
2020-11-26 21:14:58 +0100

    Add Routing to App component

--- dicksit/client/src/App.js
+++ dicksit/client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Game from './components/Game';
-import SignIn from './components/SignIn';
+import Game from './components/Game/Game';
+import SignInRouter from './components/SingIn/SignInRouter';
 
 class App extends React.Component {
   render() {
@@ -9,16 +9,13 @@ class App extends React.Component {
       <div className="App">
         <div>
           <Switch>
-            <Route 
-              exact path='/game' 
-              component={Game} 
-            />
-            <Route path='/' component={SignIn}/>
+            <Route path="/" component={SignInRouter} />
+            <Route path="/game" component={Game} />
           </Switch>
         </div>
       </div>
-  );
-}
+    );
+  }
 }
 
 export default App;

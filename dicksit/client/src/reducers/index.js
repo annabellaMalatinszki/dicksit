80535c3515950b43b5c8492a8648a0edb700ad6b
muvirag <a.malatinszki@gmail.com>
2020-12-02 14:14:00 +0100

    Add isSignedIn reducer to combiner

--- dicksit/client/src/reducers/index.js
+++ dicksit/client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import userNameReducer from './userName';
+import isSignedInReducer from './isSignedIn';
 import userColorReducer from './userColor';
 import numOfPlayersReducer from './numOfPlayers';
 
 const allReducers = combineReducers({
   userName: userNameReducer,
   userColor: userColorReducer,
+  isSignedIn: isSignedInReducer,
   numOfPlayers: numOfPlayersReducer,
 });
 

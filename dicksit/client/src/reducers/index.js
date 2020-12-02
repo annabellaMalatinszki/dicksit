c4a0cfe96d8ed7cd1432841d1aa01cecd58e47e2
muvirag <a.malatinszki@gmail.com>
2020-12-02 14:12:23 +0100

    Add numOfPlayers reducer to combiner

--- dicksit/client/src/reducers/index.js
+++ dicksit/client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userNameReducer from './userName';
 import userColorReducer from './userColor';
+import numOfPlayersReducer from './numOfPlayers';
 
 const allReducers = combineReducers({
   userName: userNameReducer,
   userColor: userColorReducer,
+  numOfPlayers: numOfPlayersReducer,
 });
 
 export default allReducers;

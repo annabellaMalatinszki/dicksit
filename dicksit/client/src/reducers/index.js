839af03f3f1b95054aa1434579df807d7e1ff788
muvirag <a.malatinszki@gmail.com>
2020-12-02 13:38:28 +0100

    Add userColor reducer to reducer combiner

--- dicksit/client/src/reducers/index.js
+++ dicksit/client/src/reducers/index.js
@@ -1,8 +1,10 @@
-import userNameReducer from './userName';
 import { combineReducers } from 'redux';
+import userNameReducer from './userName';
+import userColorReducer from './userColor';
 
 const allReducers = combineReducers({
   userName: userNameReducer,
+  userColor: userColorReducer,
 });
 
 export default allReducers;

e8a13816674f305862c474b3c6b19d55ce295760
muvirag <a.malatinszki@gmail.com>
2020-12-04 16:04:24 +0100

    Remove Grid to simplify SignIn layout

--- dicksit/client/src/components/SignIn/SignIn.js
+++ dicksit/client/src/components/SignIn/SignIn.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
 const useStyle = makeStyles({
   center: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'wrap',
   },
   button: {
     display: 'block',
@@ -21,29 +21,27 @@ const SignIn = ({ setSignInStatus }) => {
   const classes = useStyle();
 
   return (
-    <div>
-      <Grid container className={classes.center}>
-        <Button
-          className={classes.button}
-          variant="contained"
-          size="large"
-          onClick={() => {
-            setSignInStatus('host');
-          }}
-        >
-          <Typography variant="h6">Host</Typography>
-        </Button>
-        <Button
-          className={classes.button}
-          variant="contained"
-          size="large"
-          onClick={() => {
-            setSignInStatus('player');
-          }}
-        >
-          <Typography variant="h6">Join</Typography>
-        </Button>
-      </Grid>
+    <div className={classes.center}>
+      <Button
+        className={classes.button}
+        variant="contained"
+        size="large"
+        onClick={() => {
+          setSignInStatus('host');
+        }}
+      >
+        <Typography variant="h6">Host</Typography>
+      </Button>
+      <Button
+        className={classes.button}
+        variant="contained"
+        size="large"
+        onClick={() => {
+          setSignInStatus('player');
+        }}
+      >
+        <Typography variant="h6">Join</Typography>
+      </Button>
     </div>
   );
 };

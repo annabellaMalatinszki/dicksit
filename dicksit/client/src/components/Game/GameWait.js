d33cceee09943414ca939e57ef06fc7991d9fa37
muvirag <a.malatinszki@gmail.com>
2020-12-10 13:32:35 +0100

    Check if host has started the game intermittently.

--- dicksit/client/src/components/Game/GameWait.js
+++ dicksit/client/src/components/Game/GameWait.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import useInterval from '../../hooks/useInterval';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import BackgroundWrapper from '../BackgroundWrapper';
+import { getApi } from '../../requestHelper';
+import { startGame } from '../../actions';
+
+const useStyles = makeStyles({
+  progress: {
+    margin: '3em',
+  },
+});
 
 const PlayerWait = () => {
   const dispatch = useDispatch();
@@ -9,10 +20,21 @@ const PlayerWait = () => {
     state.isGameStarted;
   });
 
+  const classes = useStyles();
+
+  useInterval(async () => {
+    console.log('Checking if game has started...');
+    const res = await getApi('isgamestarted');
+    if (res.isGameStarted) {
+      dispatch(startGame(true));
+    }
+  }, 1000);
+
   return (
-    <div>
-      <CircularProgress />
-    </div>
+    <BackgroundWrapper>
+      <Typography variant="h6">Waiting for players to join</Typography>
+      <CircularProgress className={classes.progress} size="6rem" />
+    </BackgroundWrapper>
   );
 };
 

dcdc26fdcae15d81e749fc20c14acc3d3411130d
muvirag <a.malatinszki@gmail.com>
2020-12-10 13:28:56 +0100

    Return the rigth component from GameRouter depending on if the player is signed in and game has started

--- dicksit/client/src/components/Game/GameRouter.js
+++ dicksit/client/src/components/Game/GameRouter.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Game from './Game';
 import GameWait from './GameWait';
 import PleaseSignIn from './PleaseSignIn';
 
 const GameRouter = () => {
-  const isSignedIn = useSelector((state) => {
-    state.isSignedIn;
-  });
-  const isGameStarted = useSelector((state) => {
-    state.isGameStarted;
-  });
+  const isSignedIn = useSelector((state) => state.isSignedIn);
+  const isGameStarted = useSelector((state) => state.isGameStarted);
 
   let componentToRender;
+
   if (isSignedIn && isGameStarted) {
     componentToRender = <Game />;
-  } else {
+  } else if (isSignedIn) {
     componentToRender = <GameWait />;
-    //   } else {
-    //     componentToRender = <PleaseSignIn />;
+  } else {
+    componentToRender = <PleaseSignIn />;
   }
 
   return <div>{componentToRender}</div>;
